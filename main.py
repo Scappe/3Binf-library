1a9fe6636a2fa9012006cecfe3f0bf76e8fc612c
Your Name <you@example.com>
2019-10-12 07:50:49 +0200

    add int function

---
+++ main.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self,title,author,sbm)
+    self.title = title
+    self.author = author
+    self.sbn = sbn
+
+piccolo_principe = Book("Il piccolo principe", "qualcuno", 1)
+print(str(piccolo_principe))
